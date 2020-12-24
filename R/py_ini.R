3848638e6306d9885c44e2659ffcb88ec3d5293c
Michaelrising <53537769+Michaelrising@users.noreply.github.com>
2020-12-24 11:01:22 +0800

    Updata python initiate function: show errors when no cuda device

--- R/py_ini.R
+++ R/py_ini.R
@@ -1,6 +1,6 @@
 #' Initiate the reticulate environment
-#' @description A utility function to initiate the python environment and source the python function into the R environment and set it as global function. 
-#' @return the physical address of R function Power_sampling, converted from python file "python4R.py"
+#' @description A utility function to initiate the python environment , check CUDA device and source the python function into the R environment and set it as global function. 
+#' @return Error in CudaSupport means there is no cuda device or cuda driver lib cannot be found; otherwise a message of successfully set up the environment will be shown
 #' @export 
 #' @examples 
 #'\dontrun{
@@ -9,6 +9,7 @@
 py_ini <- function(){
   reticulate::py_config()
   reticulate::source_python(system.file("python","power4R.py",package="DesignCTPB"), envir = .GlobalEnv, convert = TRUE) # source python4R.py into the environment
+  reticulate::py_run_string("import numba; numba.cuda.select_device(0)", convert = TRUE)
   #assign("Power.sampling",  Power_sampling, envir = .GlobalEnv, inherits=TRUE) # set Power.sampling as an global function
-  return(Power_sampling)
+  return("Reticulate environment has been set up successfully!")
 }
